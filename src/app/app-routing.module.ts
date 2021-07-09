c55e4e7b87de31e0e3609d7a9325d9969479be98
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2021-07-09 13:30:14 -0700

    proyecto spa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import {HomeComponent} from './components/home/home.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'heroes', component: HeroesComponent},
+  {path:'about', component: AboutComponent},
+  {path:'**', pathMatch: 'full', redirectTo: 'cuerpo'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from 'src/app/services/heroes.service';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: any = {};
+  constructor(private servicioHeroes: HeroesService) {
+    this.heroes = servicioHeroes.getHeroes();
+    console.log(this.heroes);
+  }
+
+
+  ngOnInit(): void {
+   
+  }
+  
+}
